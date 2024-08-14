b95fa8b63a2c43c4e77c564eb179a61d747550b5
DavidG <GOnzalezDavid11.3.3@hotmail.com>
2024-08-13 21:52:54 -0600

    Correccion de errores  en Login

---
+++ src/Views/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Imagen from '../Components/Imagen';
+import NavBar from '../Components/NavBar';
+import AppHeader from '../Components/AppHeader';
+import '../Views/Styles/login.css'
+import { Link } from 'react-router-dom';
+
+  return (
+    <>
+      <AppHeader />
+      <NavBar />
+      <Imagen />
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div className="login-page-custom">
+          <div className="form-custom">
+            <form className="login-form-custom" method="get" onSubmit={handleSubmit}>
+              <h2>Login</h2>
+              <input type="text" placeholder="Username" required />
+              <input type="password" placeholder="Password" required />
+              <Link to='../SubIndex'>
+              <button className="btn-custom1">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Sign in
+              </button>
+              </Link>
+              <p className="message-custom">Not registered? <a href='Registro'>Create an account</a></p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
+
+export default Login;
+
